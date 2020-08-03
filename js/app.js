ce405fd6d3b2c3eefe62925a881cbb8905803b25
anchita-hari <59209132+anchita-hari@users.noreply.github.com>
2020-08-03 06:56:59 -0400

    hardcoded data with temporary filters

--- js/app.js
+++ js/app.js
@@ -1,8 +1,27 @@
-function getStatus(status) {
-    if (status == "verified") {
-        
+/* function test() {
+    var pills = document.getElementsByClassName("badge-pill");
+
+    var x = document.getElementById("status").value;
+    if (x == "verified") {
+        for (var i = 0; i < pills.length; i++) {
+            if (x !== pills[i].value) {
+                var section = document.getElementsByTagName("section");
+                var divs = section.getElementsByTagName("div");
+            }
+        }
     }
-    else {
-        
+} */
+
+function changePage() {
+    var val = document.getElementById("status").value;
+    if (val == "Verified") {
+        location.href = "verified.html";
     }
+    else if (val == "Self-Reported") {
+        location.href = "self_report.html"
+    }
+}
+
+function toHome() {
+    location.href = "index.html";
 }
