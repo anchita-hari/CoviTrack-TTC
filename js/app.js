6c01272d1fc0a6403489452b69b7241c844a730d
anchita-hari <59209132+anchita-hari@users.noreply.github.com>
2020-08-10 09:27:29 -0400

    adding option to submit new verified case

--- js/app.js
+++ js/app.js
@@ -1,27 +1,21 @@
-/* function test() {
-    var pills = document.getElementsByClassName("badge-pill");
-
-    var x = document.getElementById("status").value;
-    if (x == "verified") {
-        for (var i = 0; i < pills.length; i++) {
-            if (x !== pills[i].value) {
-                var section = document.getElementsByTagName("section");
-                var divs = section.getElementsByTagName("div");
-            }
-        }
-    }
-} */
-
 function changePage() {
-    var val = document.getElementById("status").value;
-    if (val == "Verified") {
-        location.href = "verified.html";
+    var val = document.getElementById('status').value;
+    if (val == 'Verified') {
+        location.href = 'verified.html';
     }
-    else if (val == "Self-Reported") {
-        location.href = "self_report.html"
+    else if (val == 'Self-Reported') {
+        location.href = 'self_report.html';
     }
 }
 
 function toHome() {
-    location.href = "index.html";
+    location.href = 'index.html';
+}
+
+function newCase() {
+    location.href = 'new_case.html';
+}
+
+function submitForm() {
+    alert('The case has been submitted. Thank you!'); 
 }
